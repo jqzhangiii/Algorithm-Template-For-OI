afa5a5691c278580ac9391cff7d877ca547753cb
zjqaq <33285580+zjqaq@users.noreply.github.com>
2018-04-25 22:25:09 +0800

    FFT

---
+++ Polynomial/FFT.cpp
@@ -0,0 +1,20 @@
+inline void FFT(Cp *A,int f) {
+	int i,j,k;
+	for(i=0,j=0;i<M;i++) {
+		if(i>j)swap(A[i],A[j]);
+		for(k=M>>1;(j^=k)<k;k>>=1);
+	}
+	for(i=1;i<M;i<<=1) {
+		w.x=cos(pi/i),w.y=sin(pi/i)*f;
+		for(j=0;j<M;j+=i<<1) {
+			w0.x=1,w0.y=0;
+			for(k=0;k<i;++k) {
+				x=A[j+k];
+				A[j+k]=x+(w0*A[i+j+k]);
+				A[i+j+k]=x-(w0*A[i+j+k]);
+				w0=w0*w;
+			}
+		}
+	}
+	if(f==-1) for(i=0;i<M;++i) A[i].x/=M;
+}
