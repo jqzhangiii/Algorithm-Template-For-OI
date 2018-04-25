0a8d111d52d3be974ff343ded96a36c1fe3c1496
zjqaq <33285580+zjqaq@users.noreply.github.com>
2018-04-25 15:59:46 +0800

    增量法

---
+++ String/SAM.cpp
@@ -0,0 +1,18 @@
+struct SAM {
+ 	SAM(){cnt=0;last=rt=++cnt;}
+	inline void extend(int x){
+		p = last; np = last = ++cnt; l[np] = l[p] + 1; sz[np]=1;
+		while(!c[p][x] && p) c[p][x] = np ,p = fail[p];
+		if(!p) fail[np] = rt;
+		else {
+			q = c[p][x];
+			if(l[q] == l[p] + 1) fail[np] = q;
+			else {
+				nq = ++cnt; l[nq] = l[p] + 1;
+				memcpy(c[nq],c[q],sizeof c[q]);
+				fail[nq] = fail[q] , fail[q] = fail[np] = nq;
+				while(c[p][x] == q && p) c[p][x] = nq , p = fail[p];
+			}
+		}
+	}
+}
