a7434ffa80bf3b937dbfa96937261e8d4c846186
zjqaq <33285580+zjqaq@users.noreply.github.com>
2018-06-09 16:22:12 +0800

    Update 求原根.cpp

--- Number Theory/求原根.cpp
+++ Number Theory/求原根.cpp
@@ -1,18 +1,21 @@
 //https://zh.wikipedia.org/wiki/%E5%8E%9F%E6%A0%B9
 inline fpw(int x,int y,int mod)
 
-int calc(int x)   //求原根
-{
-    if (x==2) return 1;
-    for (int i=2;i;i++)
-    {
-        bool pp=1;
-        for (int j=2;j*j<x;j++)
-            if (fpw(i,(x-1)/j,x)==1)   
-            {
-                pp=0;
-                break;
-            }
-        if (pp) return i;
-    }
-} 
+inline LL fpw(LL x,LL y,LL Mod) {
+	LL ret=1;
+	for(;y;y>>=1,x=x*x%Mod) if(y&1) ret=ret*x%Mod;
+	return ret;
+}
+
+inline int get(LL x) {
+	if(x==2) return 1;
+	for(int i=2;;++i) {
+		bool flag=1;
+		for(int j=2;j*j<x;++j) 
+			if(fpw(i,(x-1)/j,x)==1) {
+				flag=0;
+				break;
+			}
+		if(flag) return i;
+	}
+}
