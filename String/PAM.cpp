e862c6a4bedfde6ed995c2774d40b24d0edf22fa
zjqaq <33285580+zjqaq@users.noreply.github.com>
2018-06-13 17:03:33 +0800

    fix bug

--- String/PAM.cpp
+++ String/PAM.cpp
@@ -1,11 +1,11 @@
 struct PAM {
 	PAM(){cnt=f[0]=f[1]=1;l[1]=-1;}
 	inline void extend(int x,int n) {
-		int p = last; while(s[n-l[p]-1]!=s[n]) p=fail[p];
+		int p = last; while(s[n-l[p]-1]!=s[n]) p=f[p];
 		if(!c[p][x]) {
-			int now = ++cnt , k = fail[p];
+			int now = ++cnt , k = f[p];
 			l[now] = l[p] + 2;
-			while(s[n-l[k]-1]!=s[n]) k=fail[k];
+			while(s[n-l[k]-1]!=s[n]) k=f[k];
 			f[now] = c[k][x] , c[p][x] = now;
 		}
 		last = c[p][x];
